057773a1860da8b468a8f221b20bc8a2c53e6d1c
vikassharma1809 <vikasgaur9553@gmail.com>
2021-03-06 18:21:50 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
 import { GrLocation } from "react-icons/gr";
 import axios from "axios";
+import { ReactSVG } from "react-svg";
+import indiaSvg from "./static/images/india.svg";
 import "./index.scss";
 
 const App = () => {
   const [data, setData] = React.useState([]);
   const [dailyStates, setDailyStates] = React.useState([]);
-  // const { current } = React.useRef(0);
-  const confirmed = React.useRef(12345);
-  const active = React.useRef(12345);
-  const decreased = React.useRef(1234);
-  const recovered = React.useRef(122);
+  const [selectedState, setSelectedState] = React.useState(null);
+
+  let [values, setValues] = React.useState({
+    confirmed: null,
+    active: null,
+    deceased: null,
+    recovered: null,
+  });
 
   React.useEffect(() => {
     axios
@@ -29,6 +34,34 @@ const App = () => {
       .catch((err) => console.error(err));
   }, []);
 
+  const handleClick = (e) => {
+    console.log(e);
+
+    const clickedPoint = e.target;
+    if (
+      clickedPoint.closest("path") &&
+      clickedPoint.closest("path").hasAttribute("title") &&
+      clickedPoint.closest("path").getAttribute("title")
+    ) {
+      console.log(clickedPoint.closest("path").getAttribute("title"));
+      const state = clickedPoint.closest("path").getAttribute("title");
+      setSelectedState(state);
+
+      data["statewise"]?.forEach((obj) => {
+        if (obj["state"] === selectedState) {
+          setValues({
+            confirmed: obj["confirmed"],
+            active: obj["active"],
+            deceased: obj["deaths"],
+            recovered: obj["recovered"],
+          });
+        }
+      });
+    } else {
+      return;
+    }
+  };
+
   return (
     <div className="app">
       <Container fluid>
@@ -38,7 +71,7 @@ const App = () => {
             <div className="header">
               <GrLocation />
               <div className="header-text">
-                <h4>India COVID-19 Tracker</h4>
+                <h1>India Covid-19 Dashboard</h1>
                 <p>
                   Let's all pray to make our Earth Covid-19 free soon, Stay Safe
                   and do TheLocate.
@@ -51,16 +84,26 @@ const App = () => {
                 <thead>
                   <tr>
                     <th scope="col">STATE/UT</th>
-                    <th scope="col">CONFIRMED</th>
-                    <th scope="col">ACTIVE</th>
-                    <th scope="col">RECOVERED</th>
-                    <th scope="col">DECEASED</th>
+                    <th scope="col" style={{ color: "aquamarine" }}>
+                      CONFIRMED
+                    </th>
+                    <th scope="col" style={{ color: "brown" }}>
+                      ACTIVE
+                    </th>
+                    <th scope="col" style={{ color: "green" }}>
+                      RECOVERED
+                    </th>
+                    <th scope="col" style={{ color: "red" }}>
+                      DECEASED
+                    </th>
                   </tr>
                 </thead>
                 <tbody>
                   {data["statewise"]?.map((obj, i) => (
                     <tr>
-                      <th scope="row">{obj["state"]}</th>
+                      <th scope="row" style={{ color: "brown" }}>
+                        {obj["state"]}
+                      </th>
                       <td>{obj["confirmed"]}</td>
                       <td>{obj["active"]}</td>
                       <td>{obj["recovered"]}</td>
@@ -76,22 +119,34 @@ const App = () => {
           {/* map */}
           <Col xs={12} md={6}>
             <div className="header-second">
-              <h4>India Map</h4>
-              <p>HOVER OVER STATES FOR MORE DETAILS</p>
+              <h1>India Map</h1>
+              <p>Hover Over States For More Details,</p>
             </div>
 
             <Card>
-              {[
-                { label: "CONFIRMED", number: confirmed.current },
-                { label: "ACTIVE", number: active.current },
-                { label: "RECOVERED", number: recovered.current },
-                { label: "DECREASED", number: decreased.current },
-              ].map((data, index) => (
-                <div className={"colored-buttons"} key={index}>
-                  <span>{data.label}</span>
-                  <span>{data.number}</span>
-                </div>
-              ))}
+              <div className="selectedState">
+                <h3>{selectedState}</h3>
+              </div>
+              <Row noGutters>
+                <Col md={3} className="colored-buttons btn1">
+                  <span>{"CONFIRMED"}</span>
+                  <span>{values.confirmed}</span>
+                </Col>
+                <Col md={3} className="colored-buttons btn2">
+                  <span>{"ACTIVE"}</span>
+                  <span>{values.active}</span>
+                </Col>
+                <Col md={3} className="colored-buttons btn3">
+                  <span>{"RECOVERED"}</span>
+                  <span>{values.recovered}</span>
+                </Col>
+                <Col md={3} className="colored-buttons btn4">
+                  <span>{"DECEASED"}</span>
+                  <span>{values.deceased}</span>
+                </Col>
+              </Row>
+
+              <ReactSVG src={indiaSvg} onMouseOver={handleClick} />
             </Card>
           </Col>
         </Row>
